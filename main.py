504159b33498baf9fccceb16a3f52709137a79ac
hundodayso <solder-elision.0y@icloud.com>
2025-01-26 14:40:33 +0000

    1

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+import key
+import requests
+import json
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+STOCK_API_KEY = key.stock_api
+
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+
+
+stock_params = {
+    'function': 'TIME_SERIES_DAILY',
+    'symbol': STOCK,
+    'apikey': STOCK_API_KEY,
+}
+
+url = 'https://www.alphavantage.co/query?'
+response = requests.get(url,stock_params)
+if response.status_code == 200:
+    data = response.json()
+    print(data[1])
+
+else:
+    print("No response from server")
+
+data = r.json()
+
+print(data)
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
