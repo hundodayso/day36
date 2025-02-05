562eb4876398354a45bb66fefc3d5a9d17532856
hundodayso <solder-elision.0y@icloud.com>
2025-02-05 22:34:11 +0000

    day39

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+
+
+base_url = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
+
+params = {'adults': 1,
+          'departureDate': '2025-03-01',
+          'destinationLocationCode': 'TRN',
+          'originLocationCode': 'LTN'
+         }
